3aab23b0578952331a940ab93b7e45d678424dc2
ArbaazOnGithub <mohd.arbaaz.0503+github@gmail.com>
2025-05-01 20:08:59 +0530

    changed folder structure

--- N1Solution Backend/src/main/java/com/n1solution/exceptions/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.n1solution.exceptions;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
